15a87452ab2d13f101ef89b3b36e99352ec41f61
Pravs-mystic <104288377+Pravs-mystic@users.noreply.github.com>
2023-10-09 14:49:37 -0700

    modified website name

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "CuriousCappuccino",
-	url: "https://curiouscappa.in/",
+	url: "https://curiouscapp.in/",
 	language: "en",
 	description: "Brewed with curiosity and a dash of innovation, my blog serves up a steaming cup of fascinating insights and mind-blowing facts on the latest tech trends. Sip your way through the captivating aroma of our tech-based musings!",
 	author: {
 		name: "Pravalli Budida",
 		email: "pravalli.lella@gmail.com",
-		url: "https://curiouscappa.in/about-me/"
+		url: "https://curiouscapp.in/about-me/"
 	}
 }
